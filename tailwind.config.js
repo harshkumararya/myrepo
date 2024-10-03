7c651e4c8361d470d0b5fcbfcdf8711997f31d07
unknown <harshkumararya534@gmail.com>
2024-10-03 17:30:09 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'primary':'#0872BF',
+        'background':'#141A1A',
+
+      },
+    },
+  },
+  plugins: [],
+}
+
